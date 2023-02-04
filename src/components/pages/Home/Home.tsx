73342d48938365e19af6bae1e62c46faa1898ae1
Omid-Neshati <tchandnofame@gmail.com>
2023-02-04 10:01:14 +0330

    add pages component

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Khod Khat</title>
+        <meta name="description" content="Create article in English and Farsi" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>h</main>
+    </>
+  );
+}
