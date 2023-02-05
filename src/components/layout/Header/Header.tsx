6a63470245bec13624d37793a9f8d97207e48a75
Omid-Neshati <tchandnofame@gmail.com>
2023-02-05 15:13:03 +0330

    add header and footer

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <span>Brand</span>
+      <button title="NavMenu Toggle" type="button" className="bg-red-900">
+        NavMenu Toggle
+      </button>
+    </>
+  );
+}
+
+export default Header;
