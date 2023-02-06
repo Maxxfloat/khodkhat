a6eabd1f4903d307a53e67fdbb5bfefacc9947bb
Omid-Neshati <tchandnofame@gmail.com>
2023-02-06 17:00:42 +0330

    set few simple style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ import Header from './Header/Header';
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Provider>
-      <header>
+      <header className="flex justify-between h-12 m-3 md:h-14">
         <Header />
       </header>
       {children}
