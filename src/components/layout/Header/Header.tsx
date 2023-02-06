21afb82eb9c9ec25c5dea912be8a5e2805732a7f
Omid-Neshati <tchandnofame@gmail.com>
2023-02-06 17:34:58 +0330

    add header items and handle NavMenu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import useHeaderModel from './headerModel';
+import AccountPreview from './AccountPreview/AccountPreview';
+import BrandLogo from './BrandLogo';
+import ItemsContainer from './ItemsContainer';
+import NavMenuToggle from './NavMenuToggle/NavMenuToggle';
+import NavMenu from '../NavMenu/NavMenu';
 
 function Header() {
+  const { navMenuOpen, setNavMenuOpen } = useHeaderModel();
   return (
     <>
-      <span>Brand</span>
-      <button title="NavMenu Toggle" type="button" className="bg-red-900">
-        NavMenu Toggle
-      </button>
+      <ItemsContainer>
+        <NavMenuToggle navMenuOpen={navMenuOpen} setNavMenuOpen={setNavMenuOpen} />
+      </ItemsContainer>
+      <div>
+        <ItemsContainer>
+          <BrandLogo />
+        </ItemsContainer>
+      </div>
+      <ItemsContainer>
+        <AccountPreview />
+      </ItemsContainer>
+      {navMenuOpen && <NavMenu />}
     </>
   );
 }
