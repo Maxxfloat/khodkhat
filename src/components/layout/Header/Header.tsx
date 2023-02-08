d365ec755a76c860ced5d8746b072a09308a36ac
Omid-Neshati <tchandnofame@gmail.com>
2023-02-08 14:16:08 +0330

    change approach by using provider for navMenu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,24 +4,31 @@ import BrandLogo from './BrandLogo';
 import ItemsContainer from './ItemsContainer';
 import NavMenuToggle from './NavMenuToggle/NavMenuToggle';
 import NavMenu from '../NavMenu/NavMenu';
+import HeaderContext from './HeaderProvider';
 
 function Header() {
-  const { navMenuOpen, setNavMenuOpen } = useHeaderModel();
+  const { navMenuOpen, providerValue } = useHeaderModel();
   return (
-    <>
-      <ItemsContainer>
-        <NavMenuToggle navMenuOpen={navMenuOpen} setNavMenuOpen={setNavMenuOpen} />
-      </ItemsContainer>
-      <div>
+    <HeaderContext.Provider value={providerValue}>
+      <div className="flex justify-between h-12 md:h-14">
         <ItemsContainer>
-          <BrandLogo />
+          <NavMenuToggle />
+        </ItemsContainer>
+        <div>
+          <ItemsContainer>
+            <BrandLogo />
+          </ItemsContainer>
+        </div>
+        <ItemsContainer>
+          <AccountPreview />
         </ItemsContainer>
       </div>
-      <ItemsContainer>
-        <AccountPreview />
-      </ItemsContainer>
-      {navMenuOpen && <NavMenu />}
-    </>
+      <div
+        className={`flex-col w-64 mt-5 text-xl capitalize transition-transform space-y-7
+                    ${navMenuOpen ? 'translate-x-0' : '-translate-x-full pointer-events-none'}`}>
+        <NavMenu />
+      </div>
+    </HeaderContext.Provider>
   );
 }
 
