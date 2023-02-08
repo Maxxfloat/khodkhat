c2788d09b7be734a6c94dfea678c90ed0107b29a
Omid-Neshati <tchandnofame@gmail.com>
2023-02-08 15:55:16 +0330

    change style

--- src/components/layout/NavMenu/NavMenuItem.tsx
+++ src/components/layout/NavMenu/NavMenuItem.tsx
@@ -3,8 +3,8 @@ import NavMenuItemProps from '@/types/NavMenuItemProps';
 
 function NavMenuItem({ Icon, link, title }: NavMenuItemProps) {
   return (
-    <Link href={link} className="flex">
-      <Icon />
+    <Link href={link} className="flex items-center gap-3 p-3 bg-white shadow rounded-xl">
+      <Icon className="text-2xl" />
       <span>{title}</span>
     </Link>
   );
