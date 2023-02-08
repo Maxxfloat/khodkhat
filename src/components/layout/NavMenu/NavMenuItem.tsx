9aadaaf15368d2e0559801b284c79a1453b637b4
Omid-Neshati <tchandnofame@gmail.com>
2023-02-08 11:59:47 +0330

    create navMenuItem component

--- src/components/layout/NavMenu/NavMenuItem.tsx
+++ src/components/layout/NavMenu/NavMenuItem.tsx
@@ -1,5 +1,13 @@
-function NavMenuItem() {
-  return <div>NavMenuItem</div>;
+import Link from 'next/link';
+import NavMenuItemProps from '@/types/NavMenuItemProps';
+
+function NavMenuItem({ Icon, link, title }: NavMenuItemProps) {
+  return (
+    <Link href={link} className="flex">
+      <Icon />
+      <span>{title}</span>
+    </Link>
+  );
 }
 
 export default NavMenuItem;
