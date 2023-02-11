9b92881793e62549632db66716c6f940c785e9c9
Omid-Neshati <tchandnofame@gmail.com>
2023-02-11 19:31:55 +0330

    add few useless color :|

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        beige: '#FAF8F1'
+        beige: '#FAF8F1',
+        coconutCream: '#faf7e1',
+        whiteRock: '#e7e4d0',
+        foggyGray: '#ceccba',
+        merlin: '#3d3c37',
+        candlelight: '#fddf17',
+        silver: '#c5c3c3',
+        tundora: '#444444',
+        saffron: '#f4bc34',
+        brightSun: '#fcdc3c',
+        robRoy: '#ecc860',
+        gold: '#a67b00',
+        americanGold: '#d3ae36',
+        oldSilver: '#848482',
+        quickSilver: '#a6a6a6',
+        argent: '#c1c1c1'
       }
     }
   },
