c5e88c7c69ccbbcba9055517a61a29eb6e97b754
Omid-Neshati <tchandnofame@gmail.com>
2023-02-11 19:33:22 +0330

    add style

--- src/components/pages/Home/Sections/Hero/InputField.tsx
+++ src/components/pages/Home/Sections/Hero/InputField.tsx
@@ -3,10 +3,18 @@ import useInputFieldModel from './useInputFieldModel';
 function InputField() {
   const { inputValue, inputChangeHandler, postHandler } = useInputFieldModel();
   return (
-    <div>
-      <input value={inputValue} onChange={inputChangeHandler} />
-      <button type="button" onClick={postHandler}>
-        Write
+    <div className="flex flex-col items-center h-52 md:w-1/2">
+      <input
+        value={inputValue}
+        onChange={inputChangeHandler}
+        placeholder="Subject..."
+        className="p-3 m-3 text-xl border-b rounded-t shadow-md outline-none focus:border-red-600 border-merlin placeholder:text-gray-700 "
+      />
+      <button
+        type="button"
+        onClick={postHandler}
+        className="px-3 py-1 mt-2 border border-black w-fit hover:bg-white ">
+        Start
       </button>
     </div>
   );
