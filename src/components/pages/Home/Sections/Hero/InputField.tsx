aa021c4f782494cdefb4deb5ce2f2aff8d3c1e3d
Omid-Neshati <tchandnofame@gmail.com>
2023-02-12 16:43:04 +0330

    add reset button and restyle

--- src/components/pages/Home/Sections/Hero/InputField.tsx
+++ src/components/pages/Home/Sections/Hero/InputField.tsx
@@ -2,17 +2,23 @@ import SpeechInput from './SpeechInput';
 import useInputFieldModel from './useInputFieldModel';
 
 function InputField() {
-  const { inputValue, inputChangeHandler, postHandler } = useInputFieldModel();
+  const { inputValue, listening, inputChangeHandler, postHandler } = useInputFieldModel();
+
   return (
     <div className="flex flex-col items-center w-full h-52">
       <div className="flex justify-center w-full sm:w-2/3 md:w-1/2 lg:w-2/5 xl:w-1/3">
-        <input
-          value={inputValue}
-          onChange={inputChangeHandler}
-          placeholder="Subject..."
-          className="w-full p-3 m-3 text-xl border-b rounded-t shadow-md outline-none focus:border-red-600 border-merlin placeholder:text-gray-700"
-        />
-        <SpeechInput />
+        <div className="flex w-full m-3 text-xl bg-white border-b rounded-t shadow-md focus:border-red-600 border-merlin">
+          <input
+            value={inputValue}
+            onChange={inputChangeHandler}
+            placeholder="Subject..."
+            className="outline-none grow placeholder:text-gray-700 pis-3"
+          />
+          <button type="button" className="h-full p-3">
+            X
+          </button>
+        </div>
+        <SpeechInput listening={listening} />
       </div>
       <button
         type="button"
