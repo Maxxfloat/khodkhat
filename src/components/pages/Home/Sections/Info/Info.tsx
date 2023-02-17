c16811f38283448fd5f4373dd7b6e3befb61cc88
Omid-Neshati <tchandnofame@gmail.com>
2023-02-18 00:03:46 +0330

    create info section

---
+++ src/components/pages/Home/Sections/Info/Info.tsx
@@ -0,0 +1,13 @@
+function Info() {
+  return (
+    <>
+      <p>This is an info page</p>
+
+      <span className="capitalize">
+        enter your subject and get article in multi language and in every scale
+      </span>
+    </>
+  );
+}
+
+export default Info;
