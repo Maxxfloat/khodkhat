123f7b408d7911194de85ca8d54cd4ffffdd8c45
Omid-Neshati <tchandnofame@gmail.com>
2023-02-18 02:56:51 +0330

    fill parent size (relative size)

--- src/components/pages/Home/Sections/Hero/InputField.tsx
+++ src/components/pages/Home/Sections/Hero/InputField.tsx
@@ -13,25 +13,23 @@ function InputField({
     useInputFieldModel(inputValue, setInputValue);
 
   return (
-    <div className="flex flex-col items-center w-full min-h-[13rem]">
-      <div className="flex justify-center w-full sm:w-2/3 md:w-1/2 lg:w-2/5 xl:w-1/3">
-        <div className="flex w-full m-3 text-xl bg-white border-b rounded-t shadow-md focus-within:border-red-600 border-merlin">
-          <textarea
-            value={inputValue}
-            onChange={inputChangeHandler}
-            placeholder="Subject..."
-            className="py-4 border-b outline-none grow placeholder:text-gray-700 pis-3 "
-          />
-          <button
-            type="button"
-            className="self-start p-3 border-black rounded-bl border-is border-be "
-            onClick={resetHandler}>
-            X
-          </button>
-        </div>
-        <div className={`text-3xl flex items-center ${listening ? 'text-red-500' : ''}`}>
-          <SpeechInput clickMicrophoneHandler={clickMicrophoneHandler} />
-        </div>
+    <div className="flex justify-center w-full min-h-full">
+      <div className="flex w-full m-3 text-xl bg-white border-b rounded-t shadow-md focus-within:border-red-600 border-merlin">
+        <textarea
+          value={inputValue}
+          onChange={inputChangeHandler}
+          placeholder="Subject..."
+          className="py-4 border-b outline-none grow placeholder:text-gray-700 pis-3 "
+        />
+        <button
+          type="button"
+          className="self-start p-3 border-black rounded-bl border-is border-be "
+          onClick={resetHandler}>
+          X
+        </button>
+      </div>
+      <div className={`text-3xl flex items-center ${listening ? 'text-red-500' : ''}`}>
+        <SpeechInput clickMicrophoneHandler={clickMicrophoneHandler} />
       </div>
     </div>
   );
