e0e9e195c7294252f2e17d3cc82d884809227a9d
Omid-Neshati <tchandnofame@gmail.com>
2023-02-22 03:03:16 +0330

    add tones values and handle min words

--- src/components/pages/Writer/OptionsMenu/useOptionsMenuModel.ts
+++ src/components/pages/Writer/OptionsMenu/useOptionsMenuModel.ts
@@ -1,15 +1,35 @@
 import numberOfPagesCalculator from '@/utils/numberOfPagesCalculator';
 import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 
+const tones = [
+  'Formal',
+  'Informal',
+  'Pessimistic',
+  'Optimistic',
+  'Tense',
+  'Curious',
+  'Uplifting',
+  'Aggressive',
+  'Assertive',
+  'Informative',
+  'Entertaining',
+  'Sarcastic',
+  'Cooperative'
+];
+
 const useOptionsMenuModel = (words: number, setWords: Dispatch<SetStateAction<number>>) => {
   const numberOfPages = numberOfPagesCalculator(words);
 
   const wordsNumberChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const wordsNumber = e.target.value;
-    setWords(parseFloat(wordsNumber));
+    const wordsNumber = parseFloat(e.target.value);
+    if (Number.isNaN(wordsNumber) || wordsNumber < 200) {
+      setWords(200);
+    }
+    setWords(wordsNumber);
   };
 
   return {
+    tones,
     numberOfPages,
     wordsNumberChangeHandler
   };
